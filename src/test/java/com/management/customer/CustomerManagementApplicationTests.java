f27b1c7e09c2c32bfbc91703747092138157e7dd
susheelmarati <susheelmarati02@gmail.com>
2024-12-01 22:16:04 +0530

    initial commit

---
+++ src/test/java/com/management/customer/CustomerManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.management.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
